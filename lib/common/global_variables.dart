e5a6c803eb3e7ca0ce3b4c25b51f31d1314613c5
Mariana-Ulfa <mulfa1169@gmail.com>
2023-12-26 21:12:43 +0700

    integration category and product list

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,3 +1,4 @@
 class GlobalVariables {
-  static const baseUrl = 'https://flutterdev.my.id';
+  // static const baseUrl = 'https://flutterdev.my.id';
+  static const baseUrl = 'https://3559-103-105-35-113.ngrok-free.app';
 }
